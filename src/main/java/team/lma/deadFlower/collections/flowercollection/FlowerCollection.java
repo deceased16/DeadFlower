1a5a3eaaef9299fa410da274be52473d54cfc0e3
Egor <darkhype12@gmail.com>
2025-08-24 19:25:42 +0300

    add new flowers and effects

--- src/main/java/team/lma/deadFlower/collections/flowercollection/FlowerCollection.java
+++ src/main/java/team/lma/deadFlower/collections/flowercollection/FlowerCollection.java
@@ -42,23 +42,27 @@ public class FlowerCollection {
 
     static {
         collection = List.of(
-                new Flower(Material.BLUE_ORCHID, new FlowerEffect(PotionEffectType.RESISTANCE, 3)),
-                new Flower(Material.POPPY, new FlowerEffect(PotionEffectType.HEALTH_BOOST, 1)),
-                new Flower(Material.DANDELION, new FlowerEffect(PotionEffectType.SPEED, 1)),
-                new Flower(Material.ALLIUM, new FlowerEffect(PotionEffectType.REGENERATION, 1)),
-                new Flower(Material.AZURE_BLUET, new FlowerEffect(PotionEffectType.NIGHT_VISION, 1)),
-                new Flower(Material.OXEYE_DAISY, new FlowerEffect(PotionEffectType.REGENERATION, 1)),
-                new Flower(Material.CORNFLOWER, new FlowerEffect(PotionEffectType.WATER_BREATHING, 1)),
-                new Flower(Material.RED_TULIP, new FlowerEffect(PotionEffectType.STRENGTH, 1)),
+                new Flower(Material.DANDELION, new FlowerEffect(PotionEffectType.SPEED, 0)),
+                new Flower(Material.POPPY, new FlowerEffect(PotionEffectType.REGENERATION, 0)),
+                new Flower(Material.BLUE_ORCHID, new FlowerEffect(PotionEffectType.WATER_BREATHING, 1)),
+                new Flower(Material.ALLIUM, new FlowerEffect(PotionEffectType.HEALTH_BOOST, 1)),
+                new Flower(Material.AZURE_BLUET, new FlowerEffect(PotionEffectType.RESISTANCE, 1)),
+                new Flower(Material.RED_TULIP, new FlowerEffect(PotionEffectType.REGENERATION, 1)),
                 new Flower(Material.ORANGE_TULIP, new FlowerEffect(PotionEffectType.FIRE_RESISTANCE, 1)),
-                new Flower(Material.WHITE_TULIP, new FlowerEffect(PotionEffectType.JUMP_BOOST, 2)),
-                new Flower(Material.PINK_TULIP, new FlowerEffect(PotionEffectType.DOLPHINS_GRACE, 1)),
-                new Flower(Material.SUNFLOWER, new FlowerEffect(PotionEffectType.FIRE_RESISTANCE, 1)),
-                new Flower(Material.LILAC, new FlowerEffect(PotionEffectType.HASTE, 2)),
-                new Flower(Material.ROSE_BUSH, new FlowerEffect(PotionEffectType.REGENERATION, 1)),
+                new Flower(Material.WHITE_TULIP, new FlowerEffect(PotionEffectType.SPEED, 2)),
+                new Flower(Material.PINK_TULIP, new FlowerEffect(PotionEffectType.STRENGTH, 1)),
+                new Flower(Material.CORNFLOWER, new FlowerEffect(PotionEffectType.NIGHT_VISION, 0)),
+                new Flower(Material.LILY_OF_THE_VALLEY, new FlowerEffect(PotionEffectType.RESISTANCE, 2)),
+                new Flower(Material.ROSE_BUSH, new FlowerEffect(PotionEffectType.HASTE, 2)),
                 new Flower(Material.PEONY, new FlowerEffect(PotionEffectType.JUMP_BOOST, 3)),
-                new Flower(Material.LILY_OF_THE_VALLEY, new FlowerEffect(PotionEffectType.LUCK, 1)),
-                new Flower(Material.SPORE_BLOSSOM, new FlowerEffect(PotionEffectType.NIGHT_VISION, 1))
+                new Flower(Material.LILAC, new FlowerEffect(PotionEffectType.DOLPHINS_GRACE,1)),
+                new Flower(Material.SUNFLOWER, new FlowerEffect(PotionEffectType.LUCK,1)),
+                new Flower(Material.OXEYE_DAISY, new FlowerEffect(PotionEffectType.STRENGTH,1)),
+                new Flower(Material.TORCHFLOWER, new FlowerEffect(PotionEffectType.FIRE_RESISTANCE,2)),
+                new Flower(Material.PITCHER_PLANT, new FlowerEffect(PotionEffectType.ABSORPTION,1)),
+                new Flower(Material.OPEN_EYEBLOSSOM, new FlowerEffect(PotionEffectType.RESISTANCE,3)),
+                new Flower(Material.CLOSED_EYEBLOSSOM, new FlowerEffect(PotionEffectType.RESISTANCE,0)),
+                new Flower(Material.WITHER_ROSE, new FlowerEffect(PotionEffectType.WITHER,99)) // I don’t recommend using! (о_О)
         );
     }
 }
